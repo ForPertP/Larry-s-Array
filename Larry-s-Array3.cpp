a126b76f204349a6f2d4ab71682ecef40f3b93f5
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-10-28 06:57:09 +0900

    Update Larry-s-Array3.cpp

--- Larry-s-Array3.cpp
+++ Larry-s-Array3.cpp
@@ -12,3 +12,23 @@ vector<string> split(const string &);
  * The function is expected to return a STRING.
  * The function accepts INTEGER_ARRAY A as parameter.
  */
+
+string larrysArray(vector<int> A)
+{
+    int count = 0;
+    
+    for (int i = 0; i < A.size()-1; ++i)
+    {
+        for (int j = i+1; j < A.size(); ++j)
+        {
+            if (A[i] > A[j])
+            {
+                count++;
+            }
+        }
+    }
+    
+    return (count % 2 == 0) ? "YES" : "NO";
+}
+
+
