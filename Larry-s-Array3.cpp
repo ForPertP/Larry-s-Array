4f66fd49ca5d172f4dcdeb35060c950d5c047821
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-10-29 07:34:36 +0900

    Update Larry-s-Array3.cpp

--- Larry-s-Array3.cpp
+++ Larry-s-Array3.cpp
@@ -32,3 +32,41 @@ string larrysArray(vector<int> A)
 }
 
 
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        string A_temp_temp;
+        getline(cin, A_temp_temp);
+
+        vector<string> A_temp = split(rtrim(A_temp_temp));
+
+        vector<int> A(n);
+
+        for (int i = 0; i < n; i++) {
+            int A_item = stoi(A_temp[i]);
+
+            A[i] = A_item;
+        }
+
+        string result = larrysArray(A);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
