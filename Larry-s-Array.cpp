e0b82bdf11be790c1ed49290a1a74689b4d7b60c
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-11-05 04:28:29 +0900

    Update Larry-s-Array.cpp

--- Larry-s-Array.cpp
+++ Larry-s-Array.cpp
@@ -43,23 +43,6 @@ string larrysArray(vector<int> A)
     return (count % 2 == 0) ? "YES" : "NO";
 }
 
-string larrysArray2(vector<int> A)
-{
-    int count = 0;
-    
-    for (int i = 0; i < A.size()-1; ++i)
-    {
-        for (int j = i+1; j < A.size(); ++j)
-        {
-            if (A[i] > A[j])
-            {
-                count++;
-            }
-        }
-    }
-    
-    return (count % 2 == 0) ? "YES" : "NO";
-}
 
 int main()
 {
