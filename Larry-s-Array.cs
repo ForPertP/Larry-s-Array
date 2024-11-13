a6a2146e25000a461c6b6c39062281e0ded4faad
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-11-13 14:27:28 +0900

    Update Larry-s-Array.cs

--- Larry-s-Array.cs
+++ Larry-s-Array.cs
@@ -21,3 +21,29 @@ class Result
      * The function is expected to return a STRING.
      * The function accepts INTEGER_ARRAY A as parameter.
      */
+}
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> A = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(ATemp => Convert.ToInt32(ATemp)).ToList();
+
+            string result = Result.larrysArray(A);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
